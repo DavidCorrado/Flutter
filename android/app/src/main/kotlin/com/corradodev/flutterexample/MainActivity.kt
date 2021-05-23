8524cab0f11e1642afd822e49c5e9d96c78d8327
davidcorrado <davidcorrado@gmail.com>
2021-05-23 10:42:25 -0500

    Updated iOS Splash Screen and cleaned up iOS and Android Projects

---
+++ android/app/src/main/kotlin/com/corradodev/flutterexample/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.corradodev.flutterexample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/corradodev/flutterexample/flutterexample/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.corradodev.flutterexample.flutterexample
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
